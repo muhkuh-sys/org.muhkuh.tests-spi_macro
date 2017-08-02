b8789e6693b2c4a1a63abd87a45b419844f0270e
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2017-08-02 16:40:53 +0200

    Add the test template for the macro test.

--- templates/test.lua
+++ templates/test.lua
@@ -1,226 +1,258 @@
 module("test@ID@", package.seeall)
 
 require("parameters")
-require("ramtest")
+local f = require 'spi_flash_macro_test'
 
 CFG_strTestName = "@NAME@"
 
 CFG_aParameterDefinitions = {
-	{
-		name="interface",
-		default=nil,
-		help="This is the interface where the RAM is connected.",
-		mandatory=true,
-		validate=parameters.test_choice_single,
-		constrains="RAM,SDRAM_HIF,SDRAM_MEM,SRAM_HIF,SRAM_MEM"
-	},
-	{
-		name="ram_start",
-		default=nil,
-		help="Only if interface is RAM: the start of the RAM area.",
-		mandatory=false,
-		validate=parameters.test_uint32,
-		constrains=nil
-	},
-	{
-		name="ram_size",
-		default=nil,
-		help="Only if interface is RAM: The size of the RAM area in bytes.",
-		mandatory=false,
-		validate=parameters.test_uint32,
-		constrains=nil
-	},
-	{
-		name="sdram_netx",
-		default=nil,
-		help="This specifies the chip type for the parameter set.",
-		mandatory=false,
-		validate=parameters.test_uint32,
-		constrains=nil
-	},
-	{
-		name="sdram_general_ctrl",
-		default=nil,
-		help="Only if interface is SDRAM: The complete value for the netX general_ctrl register.",
-		mandatory=false,
-		validate=parameters.test_uint32,
-		constrains=nil
-	},
-	{
-		name="sdram_timing_ctrl",
-		default=nil,
-		help="Only if interface is SDRAM: The complete value for the netX timing_ctrl register.",
-		mandatory=false,
-		validate=parameters.test_uint32,
-		constrains=nil
-	},
-	{
-		name="sdram_mr",
-		default=nil,
-		help="Only if interface is SDRAM: The complete value for the netX mr register.",
-		mandatory=false,
-		validate=parameters.test_uint32,
-		constrains=nil
-	},
-	{
-		name="sdram_size_exponent",
-		default=23,
-		help="Only if interface is SDRAM: The size exponent.",
-		mandatory=true,
-		validate=parameters.test_uint32,
-		constrains=nil
-	},
-	{
-		name="sram_chip_select",
-		default=nil,
-		help="Only if interface is SRAM: The chip select of the SRAM device.",
-		mandatory=false,
-		validate=parameters.test_uint32,
-		constrains=nil
-	},
-	{
-		name="sram_ctrl",
-		default=nil,
-		help="Only if interface is SRAM: The complete value for the netX extsramX_ctrl register.",
-		mandatory=false,
-		validate=parameters.test_uint32,
-		constrains=nil
-	},
-	{
-		name="sram_size",
-		default=nil,
-		help="Only if interface is SRAM: The size of the SRAM area in bytes.",
-		mandatory=false,
-		validate=parameters.test_uint32,
-		constrains=nil
-	},
-	{
-		name="checks",
-		default="DATABUS,MARCHC,CHECKERBOARD,32BIT,BURST",
-		help="This determines which checks to run. Select one or more values from this list and separate them with commata: 08BIT, 16BIT, 32BIT and BURST.",
-		mandatory=true,
-		validate=parameters.test_choice_multiple,
-		constrains="DATABUS,08BIT,16BIT,32BIT,MARCHC,CHECKERBOARD,BURST"
-	},
-	{
-		name="loops",
-		default="1",
-		help="The number of loops to run.",
-		mandatory=true,
-		validate=parameters.test_uint32,
-		constrains=nil
-	}
+  {
+    name="unit",
+    default=nil,
+    help="This is the unit providing the SPI bus.",
+    mandatory=true,
+    validate=parameters.test_choice_single,
+    constrains="netX50_SPI0,netX50_SPI1,netX56_SQI,netX56_SPI,netX4000_SQI0,netX4000_SQI1,netX4000_SPI"
+  },
+  {
+    name="chip_select",
+    default=nil,
+    help="The chip select number on the selected SPI bus.",
+    mandatory=false,
+    validate=parameters.test_uint8,
+    constrains=nil
+  },
+  {
+    name="speed",
+    default=nil,
+    help="The speed for the SPI communication in kHz.",
+    mandatory=false,
+    validate=parameters.test_uint32,
+    constrains=nil
+  },
+  {
+    name="port_control_CS",
+    default=0xffff,
+    help="The port control value for the CS signal.",
+    mandatory=false,
+    validate=parameters.test_uint16,
+    constrains=nil
+  },
+  {
+    name="port_control_CLK",
+    default=0xffff,
+    help="The port control value for the CLK signal.",
+    mandatory=false,
+    validate=parameters.test_uint16,
+    constrains=nil
+  },
+  {
+    name="port_control_MOSI",
+    default=0xffff,
+    help="The port control value for the MOSI signal.",
+    mandatory=false,
+    validate=parameters.test_uint16,
+    constrains=nil
+  },
+  {
+    name="port_control_MISO",
+    default=0xffff,
+    help="The port control value for the MISO signal.",
+    mandatory=false,
+    validate=parameters.test_uint16,
+    constrains=nil
+  },
+  {
+    name="port_control_SIO2",
+    default=0xffff,
+    help="The port control value for the SIO2 signal.",
+    mandatory=false,
+    validate=parameters.test_uint16,
+    constrains=nil
+  },
+  {
+    name="port_control_SIO3",
+    default=0xffff,
+    help="The port control value for the SIO3 signal.",
+    mandatory=false,
+    validate=parameters.test_uint16,
+    constrains=nil
+  },
+  {
+    name="mmio_CS",
+    default=0xff,
+    help="The MMIO index for the CS signal.",
+    mandatory=false,
+    validate=parameters.test_uint8,
+    constrains=nil
+  },
+  {
+    name="mmio_CLK",
+    default=0xff,
+    help="The MMIO index for the CLK signal.",
+    mandatory=false,
+    validate=parameters.test_uint8,
+    constrains=nil
+  },
+  {
+    name="mmio_MISO",
+    default=0xff,
+    help="The MMIO index for the MISO signal.",
+    mandatory=false,
+    validate=parameters.test_uint8,
+    constrains=nil
+  },
+  {
+    name="mmio_MOSI",
+    default=0xff,
+    help="The MMIO index for the MOSI signal.",
+    mandatory=false,
+    validate=parameters.test_uint8,
+    constrains=nil
+  },
+  {
+    name="mmio_SIO2",
+    default=0xff,
+    help="The MMIO index for the SIO2 signal.",
+    mandatory=false,
+    validate=parameters.test_uint8,
+    constrains=nil
+  },
+  {
+    name="mmio_SIO3",
+    default=0xff,
+    help="The MMIO index for the SIO3 signal.",
+    mandatory=false,
+    validate=parameters.test_uint8,
+    constrains=nil
+  },
+  {
+    name="dummy_byte",
+    default=0xff,
+    help="The 8 bit value for the dummy cycles.",
+    mandatory=false,
+    validate=parameters.test_uint8,
+    constrains=nil
+  },
+  {
+    name="mode",
+    default=nil,
+    help="The SPI mode for the communication.",
+    mandatory=true,
+    validate=parameters.test_choice_single,
+    constrains="0,1,2,3"
+  },
+  {
+    name="idle_configuration",
+    default=0,
+    help="The idle configuration of the interface.",
+    mandatory=false,
+    validate=parameters.test_choice_multiple,
+    constrains="0,IO1_OE,IO1_OUT,IO2_OE,IO2_OUT,IO3_OE,IO3_OUT"
+  },
+  {
+    name="macro",
+    default=nil,
+    help="The macro to execute.",
+    mandatory=true,
+    validate=nil,
+    constrains=nil
+  }
 }
 
-function run(aParameters)
-	----------------------------------------------------------------------
-	--
-	-- Parse the parameters and collect all options.
-	--
-	
-	-- Parse the interface option.
-	local strValue = aParameters["interface"]
-	local atInterfaces = {
-		["RAM"]       = ramtest.INTERFACE_RAM,
-		["SDRAM_HIF"] = ramtest.INTERFACE_SDRAM_HIF,
-		["SDRAM_MEM"] = ramtest.INTERFACE_SDRAM_MEM,
-		["SRAM_HIF"]  = ramtest.INTERFACE_SRAM_HIF,
-		["SRAM_MEM"]  = ramtest.INTERFACE_SRAM_MEM
-	}
-	local ulInterface = atInterfaces[strValue]
-	if ulInterface==nil then
-		error(string.format("Unknown interface ID: %s", strValue))
-	end
-	
-	-- Parse the test list.
-	local astrElements = parameters.split_string(aParameters["checks"])
-	local ulChecks = 0
-	local atTests = {
-		["DATABUS"]      = ramtest.CHECK_DATABUS,
-		["08BIT"]        = ramtest.CHECK_08BIT,
-		["16BIT"]        = ramtest.CHECK_16BIT,
-		["32BIT"]        = ramtest.CHECK_32BIT,
-		["MARCHC"]       = ramtest.CHECK_MARCHC,
-		["CHECKERBOARD"] = ramtest.CHECK_CHECKERBOARD,
-		["BURST"]        = ramtest.CHECK_BURST
-	}
-	for iCnt,strElement in ipairs(astrElements) do
-		local ulValue = atTests[strElement]
-		if ulValue==nil then
-			error(string.format("Unknown check ID: %s", strElement))
-		end
-		ulChecks = ulChecks + ulValue
-	end
-	
-	local ulLoops = tonumber(aParameters["loops"])
-	
-	
-	local atRamAttributes = {
-		["interface"] = ulInterface
-	}
-	-- Check if the required parameters are present. This depends on the interface.
-	-- The RAM interface needs ram_start and ram_size.
-	if ulInterface==ramtest.INTERFACE_RAM then
-		if aParameters["ram_start"]==nil or aParameters["ram_size"] then
-			error("The RAM interface needs the ram_start and ram_size parameter set.")
-		end
-		
-		atRamAttributes["ram_start"]  = tonumber(aParameters["ram_start"])
-		atRamAttributes["ram_size"]   = tonumber(aParameters["ram_size"])
-	-- The SDRAM interfaces need sdram_general_ctrl, sdram_timing_ctrl, sdram_mr.
-	-- NOTE: The sdram_size_exponent is optional. It can be derived from the sdram_general_ctrl.
-	elseif ulInterface==ramtest.INTERFACE_SDRAM_HIF or ulInterface==ramtest.INTERFACE_SDRAM_MEM then
-		if aParameters["sdram_netx"]==nil or aParameters["sdram_general_ctrl"]==nil or aParameters["sdram_timing_ctrl"]==nil or aParameters["sdram_mr"]==nil then
-			error("The SDRAM interface needs the sdram_netx, sdram_general_ctrl, sdram_timing_ctrl and sdram_mr parameter set.")
-		end
-		
-		atRamAttributes["netX"]          = tonumber(aParameters["sdram_netx"])
-		atRamAttributes["general_ctrl"]  = tonumber(aParameters["sdram_general_ctrl"])
-		atRamAttributes["timing_ctrl"]   = tonumber(aParameters["sdram_timing_ctrl"])
-		atRamAttributes["mr"]            = tonumber(aParameters["sdram_mr"])
-		atRamAttributes["size_exponent"] = tonumber(aParameters["sdram_size_exponent"])
-	-- The SRAM interface needs sram_chip_select, sram_ctrl and sram_size.
-	elseif ulInterface==ramtest.INTERFACE_SRAM_HIF or ulInterface==ramtest.INTERFACE_SRAM_MEM then
-		if aParameters["sram_chip_select"]==nil or aParameters["sram_ctrl"]==nil or aParameters["sram_size"]==nil then
-			error("The SRAM interface needs the sram_chip_select, sram_ctrl and sram_size parameter set.")
-		end
-		
-		atRamAttributes["sram_chip_select"]  = tonumber(aParameters["sram_chip_select"])
-		atRamAttributes["sram_ctrl"]         = tonumber(aParameters["sram_ctrl"])
-		atRamAttributes["sram_size"]         = tonumber(aParameters["sram_size"])
-	else
-		error("Unknown interface ID:"..ulInterface)
-	end
-	
-	
-	----------------------------------------------------------------------
-	--
-	-- Open the connection to the netX.
-	-- (or re-use an existing connection.)
-	--
-	local tPlugin = tester.getCommonPlugin()
-	if tPlugin==nil then
-		error("No plug-in selected, nothing to do!")
-	end
-	
-	
-
-	local ulRAMStart = ramtest.get_ram_start(tPlugin, atRamAttributes)
-	local ulRAMSize  = ramtest.get_ram_size(tPlugin, atRamAttributes)
-	
-	ramtest.setup_ram(tPlugin, atRamAttributes)
-	ramtest.test_ram(tPlugin, ulRAMStart, ulRAMSize, ulChecks, ulLoops)
-	ramtest.disable_ram(tPlugin, atRamAttributes)
-	
-	
-	print("")
-	print(" #######  ##    ## ")
-	print("##     ## ##   ##  ")
-	print("##     ## ##  ##   ")
-	print("##     ## #####    ")
-	print("##     ## ##  ##   ")
-	print("##     ## ##   ##  ")
-	print(" #######  ##    ## ")
-	print("")
+local astrEscapes = {
+  ['n']='\n',
+  ['"']='"'
+}
+local function unescape(strEscaped)
+  return string.gsub(strEscaped, '\\([n"])', astrEscapes)
 end
 
+function run(aParameters)
+  ----------------------------------------------------------------------
+  --
+  -- Parse the parameters and collect all options.
+  --
+  local Logging = require 'logging'
+  local cLogger = require 'logging.console'()
+  cLogger:setLevel(Logging.DEBUG)
+
+  local cSpiFlashMacroTest = require 'spi_flash_macro_test'
+  local f = cSpiFlashMacroTest(cLogger)
+
+  -- Parse the idle configuration.
+  print(aParameters['idle_configuration'])
+  local ucIdleConfiguration = 0
+
+  -- Parse the unit.
+  local atUnits = {
+    ['netX50_SPI0'] = f.UNIT_netX50_SPI0,
+    ['netX50_SPI1'] = f.UNIT_netX50_SPI1,
+
+    ['netX56_SQI'] = f.UNIT_netX56_SQI,
+    ['netX56_SPI'] = f.UNIT_netX56_SPI,
+
+    ['netX4000_SQI0'] = f.UNIT_netX4000_SQI0,
+    ['netX4000_SQI1'] = f.UNIT_netX4000_SQI1,
+    ['netX4000_SPI'] = f.UNIT_netX4000_SPI
+  }
+  local uiUnit = atUnits[aParameters['unit']]
+
+  -- Parse the chip select.
+  local uiChipSelect = tonumber(aParameters['chip_select'])
+
+  -- Unescape the macro.
+  local strMacro = unescape(string.sub(aParameters['macro'], 2, -2))
+
+  local atSpiConfiguration = {
+    ulSpeedFifoKhz = tonumber(aParameters['speed']),
+    ulSpeedSqiRomKhz = 0,
+    ausPortControl = {
+      usCLK = tonumber(aParameters['port_control_CLK']),
+      usMOSI = tonumber(aParameters['port_control_MOSI']),
+      usMISO = tonumber(aParameters['port_control_MISO']),
+      usSIO2 = tonumber(aParameters['port_control_SIO2']),
+      usSIO3 = tonumber(aParameters['port_control_SIO3']),
+      usCS0 = tonumber(aParameters['port_control_CS'])
+    },
+    aucMmio = {
+      ucCS = tonumber(aParameters['mmio_CS']),
+      ucCLK = tonumber(aParameters['mmio_CLK']),
+      ucMISO = tonumber(aParameters['mmio_MISO']),
+      ucMOSI = tonumber(aParameters['mmio_MOSI']),
+      ucSIO2 = tonumber(aParameters['mmio_SIO2']),
+      ucSIO3 = tonumber(aParameters['mmio_SIO3']),
+    },
+    ucDummyByte = tonumber(aParameters['dummy_byte']),
+    ucMode = tonumber(aParameters['mode']),
+    ucIdleConfiguration = ucIdleConfiguration
+  }
+  local tSpiCfg = f:compile_spi_configuration(atSpiConfiguration)
+
+  local aucMacro = f:compile_macro(strMacro)
+  if aucMacro==nil then
+    error('Failed to compile the macro!')
+  end
+
+  local tTest = f:compile(tSpiCfg, uiUnit, uiChipSelect, aucMacro)
+
+  tPlugin = tester.getCommonPlugin()
+  if not tPlugin then
+    error('No plugin selected, nothing to do!')
+  end
+
+  local ulResult = tester.mbin_simple_run(nil, tPlugin, 'netx/spi_flash_macro_test_netx${ASIC_TYPE}.bin', tTest)
+  if ulResult~=0 then
+    error('The SPI macro failed.')
+  end
+
+  print("")
+  print(" #######  ##    ## ")
+  print("##     ## ##   ##  ")
+  print("##     ## ##  ##   ")
+  print("##     ## #####    ")
+  print("##     ## ##  ##   ")
+  print("##     ## ##   ##  ")
+  print(" #######  ##    ## ")
+  print("")
+end
