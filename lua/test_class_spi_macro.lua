2f990b2b11dcf1ba8712e61c21cc8c5e0aeacf9d
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-07-17 12:57:03 +0200

    Use a class for the test code.

---
+++ lua/test_class_spi_macro.lua
@@ -0,0 +1,261 @@
+local class = require 'pl.class'
+local TestClassSpiMacro = class()
+
+
+function TestClassSpiMacro:_init(strTestName)
+  self.parameters = require 'parameters'
+  self.pl = require'pl.import_into'()
+
+  self.CFG_strTestName = strTestName
+
+  self.CFG_aParameterDefinitions = {
+    {
+      name="unit",
+      default=nil,
+      help="This is the unit providing the SPI bus.",
+      mandatory=true,
+      validate=parameters.test_choice_single,
+      constrains="netX50_SPI0,netX50_SPI1,netX56_SQI,netX56_SPI,netX90_SQI,netX4000_SQI0,netX4000_SQI1,netX4000_SPI"
+    },
+    {
+      name="chip_select",
+      default=nil,
+      help="The chip select number on the selected SPI bus.",
+      mandatory=false,
+      validate=parameters.test_uint8,
+      constrains=nil
+    },
+    {
+      name="speed",
+      default=nil,
+      help="The speed for the SPI communication in kHz.",
+      mandatory=false,
+      validate=parameters.test_uint32,
+      constrains=nil
+    },
+    {
+      name="port_control_CS",
+      default=0xffff,
+      help="The port control value for the CS signal.",
+      mandatory=false,
+      validate=parameters.test_uint16,
+      constrains=nil
+    },
+    {
+      name="port_control_CLK",
+      default=0xffff,
+      help="The port control value for the CLK signal.",
+      mandatory=false,
+      validate=parameters.test_uint16,
+      constrains=nil
+    },
+    {
+      name="port_control_MOSI",
+      default=0xffff,
+      help="The port control value for the MOSI signal.",
+      mandatory=false,
+      validate=parameters.test_uint16,
+      constrains=nil
+    },
+    {
+      name="port_control_MISO",
+      default=0xffff,
+      help="The port control value for the MISO signal.",
+      mandatory=false,
+      validate=parameters.test_uint16,
+      constrains=nil
+    },
+    {
+      name="port_control_SIO2",
+      default=0xffff,
+      help="The port control value for the SIO2 signal.",
+      mandatory=false,
+      validate=parameters.test_uint16,
+      constrains=nil
+    },
+    {
+      name="port_control_SIO3",
+      default=0xffff,
+      help="The port control value for the SIO3 signal.",
+      mandatory=false,
+      validate=parameters.test_uint16,
+      constrains=nil
+    },
+    {
+      name="mmio_CS",
+      default=0xff,
+      help="The MMIO index for the CS signal.",
+      mandatory=false,
+      validate=parameters.test_uint8,
+      constrains=nil
+    },
+    {
+      name="mmio_CLK",
+      default=0xff,
+      help="The MMIO index for the CLK signal.",
+      mandatory=false,
+      validate=parameters.test_uint8,
+      constrains=nil
+    },
+    {
+      name="mmio_MISO",
+      default=0xff,
+      help="The MMIO index for the MISO signal.",
+      mandatory=false,
+      validate=parameters.test_uint8,
+      constrains=nil
+    },
+    {
+      name="mmio_MOSI",
+      default=0xff,
+      help="The MMIO index for the MOSI signal.",
+      mandatory=false,
+      validate=parameters.test_uint8,
+      constrains=nil
+    },
+    {
+      name="mmio_SIO2",
+      default=0xff,
+      help="The MMIO index for the SIO2 signal.",
+      mandatory=false,
+      validate=parameters.test_uint8,
+      constrains=nil
+    },
+    {
+      name="mmio_SIO3",
+      default=0xff,
+      help="The MMIO index for the SIO3 signal.",
+      mandatory=false,
+      validate=parameters.test_uint8,
+      constrains=nil
+    },
+    {
+      name="dummy_byte",
+      default=0xff,
+      help="The 8 bit value for the dummy cycles.",
+      mandatory=false,
+      validate=parameters.test_uint8,
+      constrains=nil
+    },
+    {
+      name="mode",
+      default=nil,
+      help="The SPI mode for the communication.",
+      mandatory=true,
+      validate=parameters.test_choice_single,
+      constrains="0,1,2,3"
+    },
+    {
+      name="idle_configuration",
+      default=0,
+      help="The idle configuration of the interface.",
+      mandatory=false,
+      validate=parameters.test_choice_multiple,
+      constrains="0,IO1_OE,IO1_OUT,IO2_OE,IO2_OUT,IO3_OE,IO3_OUT"
+    },
+    {
+      name="macro",
+      default=nil,
+      help="The file name of the macro to execute.",
+      mandatory=true,
+      validate=nil,
+      constrains=nil
+    }
+  }
+end
+
+
+
+function TestClassSpiMacro:run(aParameters, tLog)
+  ----------------------------------------------------------------------
+  --
+  -- Parse the parameters and collect all options.
+  --
+  local cSpiMacroTest = require 'spi_macro_test'
+  local f = cSpiMacroTest(tLog)
+
+  -- Parse the idle configuration.
+  print(aParameters['idle_configuration'])
+  local ucIdleConfiguration = 0
+
+  -- Parse the unit.
+  local atUnits = {
+    ['netX50_SPI0'] = f.UNIT_netX50_SPI0,
+    ['netX50_SPI1'] = f.UNIT_netX50_SPI1,
+
+    ['netX56_SQI'] = f.UNIT_netX56_SQI,
+    ['netX56_SPI'] = f.UNIT_netX56_SPI,
+
+    ['netX90_SQI'] = f.UNIT_netX90_SQI,
+
+    ['netX4000_SQI0'] = f.UNIT_netX4000_SQI0,
+    ['netX4000_SQI1'] = f.UNIT_netX4000_SQI1,
+    ['netX4000_SPI'] = f.UNIT_netX4000_SPI
+  }
+  local uiUnit = atUnits[aParameters['unit']]
+
+  -- Parse the chip select.
+  local uiChipSelect = tonumber(aParameters['chip_select'])
+
+  -- Read the macro file.
+  local strFileName = self.pl.path.exists(aParameters['macro'])
+  if strFileName==nil then
+    tLog.error('The macro file "%s" does not exist.', aParameters['macro'])
+    error('Failed to load the macro.')
+  end
+  local strMacro = self.pl.file.read(strFileName)
+
+  local atSpiConfiguration = {
+    ulSpeedFifoKhz = tonumber(aParameters['speed']),
+    ulSpeedSqiRomKhz = 0,
+    ausPortControl = {
+      usCLK = tonumber(aParameters['port_control_CLK']),
+      usMOSI = tonumber(aParameters['port_control_MOSI']),
+      usMISO = tonumber(aParameters['port_control_MISO']),
+      usSIO2 = tonumber(aParameters['port_control_SIO2']),
+      usSIO3 = tonumber(aParameters['port_control_SIO3']),
+      usCS0 = tonumber(aParameters['port_control_CS'])
+    },
+    aucMmio = {
+      ucCS = tonumber(aParameters['mmio_CS']),
+      ucCLK = tonumber(aParameters['mmio_CLK']),
+      ucMISO = tonumber(aParameters['mmio_MISO']),
+      ucMOSI = tonumber(aParameters['mmio_MOSI']),
+      ucSIO2 = tonumber(aParameters['mmio_SIO2']),
+      ucSIO3 = tonumber(aParameters['mmio_SIO3']),
+    },
+    ucDummyByte = tonumber(aParameters['dummy_byte']),
+    ucMode = tonumber(aParameters['mode']),
+    ucIdleConfiguration = ucIdleConfiguration
+  }
+  local tSpiCfg = f:compile_spi_configuration(atSpiConfiguration)
+
+  local aucMacro = f:compile_macro(strMacro)
+  if aucMacro==nil then
+    error('Failed to compile the macro!')
+  end
+
+  local tTest = f:compile(tSpiCfg, uiUnit, uiChipSelect, aucMacro)
+
+  tPlugin = tester.getCommonPlugin()
+  if not tPlugin then
+    error('No plugin selected, nothing to do!')
+  end
+
+  local ulResult = tester.mbin_simple_run(nil, tPlugin, 'netx/spi_macro_test_netx${ASIC_TYPE}.bin', tTest)
+  if ulResult~=0 then
+    error('The SPI macro failed.')
+  end
+
+  print("")
+  print(" #######  ##    ## ")
+  print("##     ## ##   ##  ")
+  print("##     ## ##  ##   ")
+  print("##     ## #####    ")
+  print("##     ## ##  ##   ")
+  print("##     ## ##   ##  ")
+  print(" #######  ##    ## ")
+  print("")
+end
+
+return TestClassSpiMacro
