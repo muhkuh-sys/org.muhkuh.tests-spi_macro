9577f66209b7c046889cbb82f5361af7af7d5383
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2020-07-20 17:06:08 +0200

    Reserve space for the parameter header.

--- src/parameter_placeholder.c
+++ src/parameter_placeholder.c
@@ -1,4 +1,7 @@
 #include "main.h"
 
+/* This is a placeholder for the header. */
+unsigned long aulParameterHeaderPlaceholder[3] __attribute__ ((section (".parameter")));
+
 /* This is a placeholder to reserve enough space for the parameter area. */
 TEST_PARAMETER_T tParameterPlaceholder __attribute__ ((section (".parameter")));
