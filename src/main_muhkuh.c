ce33f6c194ff90b630452813bbc935ca61d9689a
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2026-04-22 11:29:24 +0200

    Remove obsolete file.

--- src/main_muhkuh.c
+++
@@ -1,9 +0,0 @@
-#include "flash_test.h"
-
-
-
-TEST_RESULT_T test_main_muhkuh(TEST_PARAMETER_T *ptParameter);
-TEST_RESULT_T test_main_muhkuh(TEST_PARAMETER_T *ptParameter)
-{
-	return flash_test(ptParameter);
-}
